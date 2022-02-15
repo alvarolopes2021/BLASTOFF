a28763da802944af953e3f92d5081ccd94dae928
alvaro <alvarolopes2020@protonmail.com>
2022-02-15 18:22:24 -0300

    feat: created exercise 1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blastoff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("###   TOODOO - BLASTOFF ###   \n \n");
+
+            Exercise1();
+        }
+
+        private static void Exercise1(){
+            Console.WriteLine("###   Exercise 1   ### \n");
+
+            List<string> ageVariables = new List<string>{"I", "J", "K", "X", "Y"};
+            Dictionary<string, int> ages = new Dictionary<string, int>();
+            float avg = 0;
+
+            for(int i = 0; i < ageVariables.Count; i++){
+
+                int age = 0;
+
+                do{                        
+                    bool success = int.TryParse(Console.ReadLine(), out age);
+                    if(!success)                
+                        Console.WriteLine("Value must be a number");
+                }while(age <= 0);
+
+                ages.Add(ageVariables[i], age);
+                avg = avg + age;
+            }
+
+            Console.WriteLine($"The average age is: {avg/ages.Count}");
+            Console.ReadLine();
+
+            Exercise2();
+        }
+
+        private static void Exercise2(){
+             
+        }
+    }
+}
---
+++ README.md.backup
@@ -0,0 +1,14 @@
+# TOODOO - BLASTOFF
+
+## Repositório destinado aos exercícios de lógica da Toodoo
+
+Como rodar o projeto Blastoff:
+
+* Tenha instalado o .NET em sua máquina [.NET](https://dotnet.microsoft.com/en-us/download ".NET")
+
+* Tenha o Git instalado em sua máquina [Git](https://git-scm.com/ "Git")
+
+* Com o Git, clone este repositório em sua máquina, rodando o comando: *git clone https://github.com/alvarolopes2021/BLASTOFF.git*
+
+* Utilizando um terminal, seja do git ou outro, rode o comando, dotnet run (no diretório do repositório clonado)
+

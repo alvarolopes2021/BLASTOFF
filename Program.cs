1af753ab2170519d2c1d775bf8e68137d6f45487
alvaro <alvarolopes2020@protonmail.com>
2022-02-16 16:05:48 -0300

    feat: added exercise 5

--- Program.cs
+++ Program.cs
@@ -125,7 +125,56 @@ namespace Blastoff
         }
 
         private static void Exercise4(){
-            
+            Console.WriteLine("###   Exercise 4   ### \n");
+
+            double tempCelcius = 0;
+
+            do{
+                Console.Write("Type the temperature in Celcius: ");
+
+                bool success = double.TryParse(Console.ReadLine(), out tempCelcius);
+                if(!success)
+                    Console.WriteLine("The temperature must be a number!");
+                else if(tempCelcius<-273.15)
+                    Console.WriteLine("Impossible temperature");
+
+            }while(tempCelcius <-273.15);
+
+            double tempF = 32 + tempCelcius;
+
+            Console.WriteLine($"The temperature in fahrenheit is: {tempF}");
+
+            Console.ReadLine();
+
+            Exercise5();
+
+        }
+
+
+        private static void Exercise5(){
+            Console.WriteLine("###   Exercise 5   ### \n");
+
+            double num1, num2;
+
+            bool success= false;
+            do{
+                Console.Write("Type a number: ");
+                success = double.TryParse(Console.ReadLine(), out num1);                
+            }while(!success);
+
+            do{
+                Console.Write("Type a number: ");
+                success = double.TryParse(Console.ReadLine(), out num2);                
+            }while(!success);
+
+            bool areMultiple = (num1 % num2 == 0);
+
+            if(areMultiple)
+                Console.WriteLine($"{num1} and {num2} are multiple");
+            else    
+                Console.WriteLine($"{num1} and {num2} are not multiple");
+
+            Console.ReadLine();
         }
     }
 }
